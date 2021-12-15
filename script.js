afcfbfd29d93e3b527d3a081c5dc0aff4e9cb2b8
marloncsoza <95593878+marloncsoza@users.noreply.github.com>
2021-12-15 16:40:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//FAQ JS
-// Code By Webdevtrick ( https://webdevtrick.com )
-const items = document.querySelectorAll(".accordion a");
- 
-function toggleAccordion(){
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
- 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 
 
@@ -44,9 +34,6 @@ function toggleAccordion(){
 }
  
 items.forEach(item => item.addEventListener('click', toggleAccordion));
-
-
-
 
 
 
