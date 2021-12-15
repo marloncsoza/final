cf8bbb39e4201b308c64aa63f192b3dddc698acd
marloncsoza <95593878+marloncsoza@users.noreply.github.com>
2021-12-15 16:48:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,47 @@ function createCard(weatherList){
 
 
 
+
+
+/* set copyright date and current date*/
+
+try{
+    const options = { 
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+              
+    }; 
+    const theYear = {
+        year: 'numeric'
+    }
+    document.getElementById("currentDate").textContent = new Date().toLocaleDateString("en-UK", options);
+    document.getElementById("year").textContent = new Date().toLocaleDateString("en-UK", theYear);
+
+}
+catch(e){
+    alert("error with code or your browser does not support Locale");
+}
+
+
+
+//FAQ JS
+// Code By Webdevtrick ( https://webdevtrick.com )
+const items = document.querySelectorAll(".accordion a");
+ 
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+ 
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+
+
+
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
